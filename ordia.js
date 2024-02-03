8880b5f039ae199b8b47460d21aa1a2ee4319a1f
Osama Eid <63171335+osamaeid908@users.noreply.github.com>
2024-02-03 21:25:34 +0300

    Add files via upload

---
+++ ordia.js
@@ -0,0 +1,164 @@
+// http://stackoverflow.com/questions/1026069/
+function capitalizeFirstLetter(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+
+function convertDataTableData(data, columns, linkPrefixes={}) {
+    // Handle 'Label' columns.
+
+    // var linkPrefixes = (options && options.linkPrefixes) || {};
+    
+    var convertedData = [];
+    var convertedColumns = [];
+    for (var i = 0 ; i < columns.length ; i++) {
+	column = columns[i];
+	if (column.substr(-11) == 'Description') {
+	    convertedColumns.push('description');
+	} else if (column.substr(-5) == 'Label') {
+	    // pass
+	} else if (column.substr(-3) == 'Url') {
+	    // pass
+	} else {
+	    convertedColumns.push(column);
+	}
+    }
+    for (var i = 0 ; i < data.length ; i++) {
+	var convertedRow = {};
+	for (var key in data[i]) {
+	    if (key.substr(-11) == 'Description') {
+		convertedRow['description'] = data[i][key];
+
+	    } else if (key.substr(-5) == 'image') {
+		convertedRow[key] = '<img src="' + data[i][key] + '">';
+
+	    } else if (key + 'Label' in data[i]) {
+		convertedRow[key] = '<a href="' +
+		    (linkPrefixes[key] || "") + 
+		    data[i][key].substr(31) +
+		    '">' + data[i][key + 'Label'] + '</a>';
+	    } else if (key.substr(-5) == 'Label') {
+		// pass
+		
+	    } else if (key + 'Url' in data[i]) {
+		if (data[i][key + 'Url']) {
+		    convertedRow[key] = '<a href="' +
+			data[i][key + 'Url'] +
+			'">' + data[i][key] + '</a>';
+		}
+		else {
+		    // If the URL is empty we do not create a link
+		    convertedRow[key] = data[i][key];
+		}
+	    } else if (key.substr(-3) == 'Url') {
+		// pass
+
+	    } else if (key.substr(-3) == 'url') {
+		// Convert URL to a link
+		convertedRow[key] = "<a href='" +
+		    data[i][key] + "'>" + 
+		    $("<div>").text(data[i][key]).html() + '</a>';
+
+	    } else {
+		convertedRow[key] = data[i][key];
+	    }
+	}
+	convertedData.push(convertedRow);
+    }
+    return {data: convertedData, columns: convertedColumns}
+}
+
+
+function entityToLabel(entity, language='en') {
+    if (language in entity['labels']) {
+        return entity['labels'][language].value;
+    }
+
+    // Fallback
+    languages = ['en', 'da', 'de', 'es', 'fr', 'jp',
+                 'nl', 'no', 'ru', 'sv', 'zh'];
+    for (lang in languages) {
+        if (lang in entity['labels']) {
+            return entity['labels'][lang].value;
+        }
+    }
+
+    // Last resort
+    return entity['id']
+}
+
+
+function sparqlDataToSimpleData(response) {
+    // Convert long JSON data from from SPARQL endpoint to short form
+    let data = response.results.bindings;
+    let columns = response.head.vars
+    var convertedData = [];
+    for (var i = 0 ; i < data.length ; i++) {
+	var convertedRow = {};
+	for (var key in data[i]) {
+	    convertedRow[key] = data[i][key]['value'];
+	}
+	convertedData.push(convertedRow);
+    }
+    return {data: convertedData, columns: columns};
+}
+
+
+function sparqlToDataTable(sparql, element, options={}) {
+    // Options: linkPrefixes={}, paging=true
+    var linkPrefixes = (typeof options.linkPrefixes === 'undefined') ? {} : options.linkPrefixes;
+    var paging = (typeof options.paging === 'undefined') ? true : options.paging;
+    var sDom = (typeof options.sDom === 'undefined') ? 'lfrtip' : options.sDom;
+    
+    var post_url = "https://query.wikidata.org/sparql";
+    var post_data = "query=" + encodeURIComponent(sparql) + '&format=json'
+    
+    $.post(post_url, post_data, function(response) {
+	var simpleData = sparqlDataToSimpleData(response);
+
+	convertedData = convertDataTableData(simpleData.data, simpleData.columns, linkPrefixes=linkPrefixes);
+	columns = [];
+	for ( i = 0 ; i < convertedData.columns.length ; i++ ) {
+	    var column = {
+		data: convertedData.columns[i],
+		title: capitalizeFirstLetter(convertedData.columns[i]).replace(/_/g, "&nbsp;"),
+		defaultContent: "",
+	    }
+	    columns.push(column)
+	}
+
+	table = $(element).dataTable({ 
+	    data: convertedData.data,
+	    columns: columns,
+	    lengthMenu: [[10, 25, 100, -1], [10, 25, 100, "All"]],
+	    ordering: true,
+	    order: [], 
+	    paging: paging,
+	    sDom: sDom,
+	});
+
+	$(element).append(
+	    '<caption><a href="https://query.wikidata.org/#' + 
+		encodeURIComponent(sparql) +	
+		'">Edit on query.Wikidata.org</a></caption>');
+    }, "json");
+}
+
+function qToWembedderToDataTable(q, sparql, element, options={}) {
+    var wembedderUrl = "https://wembedder.toolforge.org/api/most-similar/" + q;
+    $.ajax({
+	url: wembedderUrl,
+	error: function(xhr, status, error) { $(element).append(error); },
+	success: function (data) {
+	    
+	    var values = "";
+	    data.most_similar.forEach(function(entry, idx, array) {
+		values += "(wd:" + entry.item + " " + entry.similarity + ") ";
+	    });
+	    
+	    var interpolated_sparql = sparql.replace(/#VALUES/g, values); 
+	    
+	    sparqlToDataTable(interpolated_sparql, element, options={}); 
+	},
+    });
+}
